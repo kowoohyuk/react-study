e0eda8f7fc79c2733a53b3e4127b7c1838b4a8db
kowoohyuk <kowoohyuk91@gmail.com>
2020-05-14 23:57:39 +0900

    useReducer

--- 08/src/App.js
+++ 08/src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import './App.css';
-import Counter from './Counter';
-// import Info from './Info';
+import React from "react";
+import "./App.css";
+// import Counter from './Counter';
+import Info from "./Info";
 
-const App = () => <Counter />;
+// const App = () => <Counter />;
+const App = () => <Info />;
 // const App = () => {
 //   const [visible, setVisible] = useState(false);
 
--- 08/src/Info.js
+++ 08/src/Info.js
@@ -1,47 +1,29 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
 
-  const onChangeName = e => {
-    setName(e.target.value);
-  }
+  const { name, nickname } = state;
 
-  const onChangeNickname = e => {
-    setNickname(e.target.value);
-  }
-
-  //useEffect 실행: 마운트, 업데이트
-  /*
-  useEffect(() => {
-    console.log(
-      `렌더링이 완료되었습니다.
-      이름: ${name}
-      닉네임: ${nickname}`
-    );
-  // }, []); // 두 번째 파라미터에 빈 배열을 추가하면 마운트 시에만 실행.
-  }, [name]); // 두 번째 파라미터에 특정 객체를 추가하면 해당 값이 수정될 때 실행.
-  */
-
-  useEffect(() => {
-    console.log('effect');
-    console.log(name);
-    return () => {
-      // 렌더링 될 때 실행 (뒷정리 함수)
-      // 업데이트 직전의 값을 출력
-      console.log('cleanup');
-      console.log(name);
-    }
-  }, []); // 두 번째 파라미터에 빈 배열을 추가하면 마운트 시에만 실행.
-
-  useReducer
+  const onChange = (e) => {
+    dispatch(e.target);
+  };
 
   return (
     <>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
