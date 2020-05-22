8ffb79f86d5dbad16459d41c5f0effa4a1b0bcda
kowoohyuk <kowoohyuk91@gmail.com>
2020-05-22 18:23:45 +0900

    09

--- 09/src/StyledComponent.js
+++ 09/src/StyledComponent.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
  
+const sizes = {
+  pc: 1024,
+  tablet: 768
+};
+
+const media = Object.keys(sizes).reduce((acc, cur) => {
+  acc[cur] = (...args) => css`
+    @media (max-width: ${sizes[cur] / 16}em) {
+      ${css(...args)};
+    }
+  `
+  return acc;
+}, {});
+
+console.log(media);
+
 const Box = styled.div`
   /* props로 넣어 준 값을 직접 전달해 줄 수 있다. */
   background: ${props => props.color || 'blue'};
   padding: 1rem;
   display: flex;
+  ${media.pc`width: 768px;`};
+  ${media.tablet`width: 100%;`};
 `;
 
 const Button = styled.button`
