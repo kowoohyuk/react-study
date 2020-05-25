c0be0ee4b7d7ac56a51c67d0c5a47ce22a13cc2e
kowoohyuk <kowoohyuk91@gmail.com>
2020-05-25 18:38:58 +0900

    11. useState 함수형 업데이트 사용

---
+++ 11/src/components/TodoInsert.js
@@ -0,0 +1,34 @@
+import React, { useState, useCallback } from 'react';
+import { MdAdd } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+  const onchange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+
+      // submit 이벤트의 새로고침 방지
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onchange}
+      />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
