a265bc91d000e4446581bc6e9d1b9a9561aa09db
kowoohyuk <kowoohyuk91@gmail.com>
2020-05-28 18:02:10 +0900

    13

--- 13/src/About.js
+++ 13/src/About.js
@@ -1,10 +1,16 @@
 import React from 'react';
- 
-const About = () => {
+import qs from 'qs';
+
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true // 프리픽스인 ?를 생략 시 사용
+  });
+  const showDetail = query.detail === 'true';
   return (
     <div>
       <h1>리액트 라우터소개</h1>
       <p>라우터에요!</p>
+      {showDetail && <p>detail 값을 true로 설정하셨군요!</p>}
     </div>
   );
 };
--- 13/src/App.js
+++ 13/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 const App = () => {
   return (
@@ -13,10 +15,28 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">히스토리</Link>
+        </li>
       </ul>
       <hr />
-      <Route path="/" component={Home} exact={true}/>
-      <Route path={['/about','/info']} component={About} />
+      <Switch>
+        <Route path="/" component={Home} exact={true}/>
+        <Route path={['/about','/info']} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   )
 };
