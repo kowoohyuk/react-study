3ed0f2aadadd542a58ac797771f528c11a23f2ff
kowoohyuk <kowoohyuk91@gmail.com>
2020-06-02 18:05:23 +0900

    17.5

--- 17/src/index.js
+++ 17/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
